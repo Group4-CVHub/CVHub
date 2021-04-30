e91f8b0298ed3d376f458b0bb3dccba184ec8372
Tobias Nyholm <nyholm.tobias@gmail..se>
2021-04-30 13:31:50 +0200

    Går att logga in och inloggnings knapp försvinner om användaren redan är inloggad.

--- CvHub/Controllers/ValidationController.cs
+++ CvHub/Controllers/ValidationController.cs
@@ -5,12 +5,14 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.Facebook;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace CvHub.Controllers
 {
-
+    [Authorize]
     public class ValidationController : Controller
     {
         private readonly ApplicationDbContext _db;
@@ -29,23 +31,39 @@ namespace CvHub.Controllers
         [AllowAnonymous]
         public IActionResult FacebookSignIn()
         {
-            //Magic happens... Systemet kollar om man är inloggad, annars skickar den vidare till "FacebookValidation".
+            //Skapar ett objekt med authentication properties och sätter redirectURL till facebook validerings sidan.
             var properties = new AuthenticationProperties
             {
                 RedirectUri = Url.Action("FacebookValidation")
             };
 
+            //Magic happens... Metoden matchar properties objektet med Facebooks standard authentication Scheme om det inte matchar skickas användaren vidare till 
+            //inloggning på facebook via facebooks API. Svaret skickas sedan in i "FacebookValidation" som är satt som redirectURL i properties.
             return Challenge(properties, FacebookDefaults.AuthenticationScheme);
         }
 
         public async Task<IActionResult> FacebookValidation()
         {
-            //Detta är Http svaret från facebook som är lagrat i Cookies i webbläsaren. 
+            //Detta är Http svaret från facebook som är lagrat som Cookies i webbläsaren. 
             var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
 
-            //Detta tar Http svaret och gör om det till Json
+            //additional magic... Detta tar Http svaret och delar upp svarets olika delar och skjuter in dem i listan results.
             var results = result.Principal.Identities.FirstOrDefault().Claims.ToList();
+            
+            if (ValidateUser(results) == 1)
+            {
+                return View("SignIn", ValidateUser(results));
+            }
+            else
+            {
+                var properties = new AuthenticationProperties();
+                properties.ExpiresUtc = System.DateTime.UtcNow;
+                return BadRequest();
+            }
+        }
 
+        private int ValidateUser(List<Claim> results)
+        {
             User user = new User();
 
             //Här letade jag upp rätt element med hjälp av ElementAt och sedan gjorde jag om det till en sträng.
@@ -53,13 +71,6 @@ namespace CvHub.Controllers
             //Till sist så görs alla karraktärer till små bokstäver för att generallisera resultet. 
             user.FirstName = results.ElementAt(3).ToString().Remove(0, 65).ToLower();
             user.LastName = results.ElementAt(4).ToString().Remove(0, 63).ToLower();
-
-            return Json(Validation(user));
-
-        }
-
-        public int Validation(User user)
-        {
 
             var DbObject = _db.Users
                             .Where(u => u.FirstName.ToLower() == user.FirstName && u.LastName.ToLower() == user.LastName)
@@ -71,8 +82,15 @@ namespace CvHub.Controllers
             }
             else
             {
-                DbObject.ValidationToken = 1;
-                return 1;
+                if (DbObject.ValidationToken == 0)
+                {
+                    DbObject.ValidationToken = 1;
+                    return 1;
+                }
+                else
+                {
+                    return 1;
+                }
             }
         }
     }
