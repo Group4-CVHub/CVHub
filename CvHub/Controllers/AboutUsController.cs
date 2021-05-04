4c45ab2b24d928bde336016b4e70f0f5768ec4f7
Your NameMoreno092 <you@example.com>
2021-05-04 10:10:06 +0200

    About us

---
+++ CvHub/Controllers/AboutUsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CvHub.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        [Route("AboutUs")]
+        [Route("AboutUs/Indext")]
+        [Route("AboutUs/Indext/{id?}")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
